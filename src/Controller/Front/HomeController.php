6ff724049aed3aaa14a5a51c8b6412363e92a234
unknown <florian@agence-cwa.fr>
2023-01-10 18:52:02 +0100

    Save progression

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Trick;
 use App\Repository\CommentRepository;
 use App\Repository\TrickRepository;
 use App\Repository\UserRepository;
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,20 +16,40 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name= "homepage")
      * @param TrickRepository $tricksRepository
+     * @param ImageRepository $imagesRepository
      * @param UserRepository $usersRepository
      * @param CommentRepository $commentsRepository
      * @return Response
      */
-    public function index(TrickRepository $tricksRepository, UserRepository $usersRepository, CommentRepository $commentsRepository): Response
+    public function index(TrickRepository $tricksRepository, ImageRepository $imagesRepository, UserRepository $usersRepository, CommentRepository $commentsRepository): Response
     {
         $isAuthenticated = false;
 
         session_start();
 
         $tricks = $tricksRepository->findAll();
+        $images = $imagesRepository->findAll();
+        
+        // dd($tricks);
+        // foreach ($tricks as $trick) {
+            // dump($trick);
+            // foreach ($images as $image) {
+
+                // if ($trick->getId() === $image->getId()) {
+
+                // }
+                // dump($image);
+            // }
+        // }
+
+
+
+        // die();
+        // dd($tricks, $images);
+
         $users = $usersRepository->findAll();
         $comments = $commentsRepository->findAll();
 
-        return $this->render('homepage/homepage.html.twig', ['tricks'=>$tricks, 'users'=>count($users), 'comments'=>count($comments), 'is_authenticated'=>$isAuthenticated]);
+        return $this->render('homepage/homepage.html.twig', ['tricks'=>$tricks, 'images'=>$images, 'users'=>count($users), 'comments'=>count($comments), 'is_authenticated'=>$isAuthenticated]);
     }
 }
